70885b334a4a25dcda76db6f895b133fae2096f0
dvnoe7 <dvnoeedu@gmail.com>
2026-02-03 15:30:19 +0100

    Create controllability_observability.jl

---
+++ src/image_abstractions/controllability_observability.jl
@@ -0,0 +1,43 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+#> [frontmatter]
+#> chapter = 1
+#> video = "https://www.youtube.com/watch?v=3zTO3LEY-cM"
+#> image = ""
+#> section = 3
+#> order = 3
+#> title = "Controllability and Observabilityn"
+#> layout = "layout.jlhtml"
+#> youtube_id = "3zTO3LEY-cM"
+#> description = ""
+#> tags = ["lecture", "module1", "track_julia", "track_math"]
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c71e8450-010b-11f1-b537-474c7c2e6771
+md"""
+# Controllability and Observability
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.4"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─c71e8450-010b-11f1-b537-474c7c2e6771
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
