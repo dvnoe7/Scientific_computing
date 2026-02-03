06fae5cb780ab0103f97e5878c9dd05ba1dad814
dvnoe7 <dvnoeedu@gmail.com>
2026-02-03 14:37:35 +0000

    Deploying to gh-pages from @ dvnoe7/Scientific_computing@f67f3ff5ec35561f3c05df4d82e862edb9aed0eb 🚀

---
+++ generated_assets/controllability_observability_4b9d33b6.jl
@@ -0,0 +1,42 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+#> [frontmatter]
+#> chapter = 1
+#> image = ""
+#> section = 3
+#> order = 3
+#> title = "Controllability and Observability"
+#> layout = "layout.jlhtml"
+#> youtube_id = "3zTO3LEY-cM"
+#> description = ""
+#> tags = ["lecture", "module1", "track_julia", "track_math"]
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c71e8450-010b-11f1-b537-474c7c2e6771
+md"""
+# Controllability and Observability
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.4"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─c71e8450-010b-11f1-b537-474c7c2e6771
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
