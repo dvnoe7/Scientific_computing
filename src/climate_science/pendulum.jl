fedd6d73b05746c94a90dc046108492d54c51988
dvnoe7 <dvnoeedu@gmail.com>
2026-02-20 10:54:08 +0100

    change module

--- src/climate_science/pendulum.jl
+++ src/climate_science/pendulum.jl
@@ -7,7 +7,7 @@
 #> order = 6
 #> title = "Control of Pendulum on a cart"
 #> layout = "layout.jlhtml"
-#> tags = ["lecture", "module1", "track_julia", "track_math"]
+#> tags = ["lecture", "module3", "track_julia", "track_math"]
 
 using Markdown
 using InteractiveUtils
